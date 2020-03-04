894432b4aa6545e8080295fb1a04e3c6bea73de7
vskumbhar <vskumbhar@BAF-LAP-903557.BFLAF.COM>
2020-03-04 16:02:26 +0530

    MysqlDBConnection Created

---
+++ TestProject/src/com/org/bafl/db/MysqlDBConnection.java
@@ -0,0 +1,10 @@
+package com.org.bafl.db;
+
+public class MysqlDBConnection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java World");
+	}
+
+}
